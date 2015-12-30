086dc4a29155f3a37e236baaaef80c7e5992b6e5
Kensuke Kosaka <k@trileg.net>
2015-12-30 22:53:39 +0900

    Add ping-pong function, use environment variable to manage api key.

--- README.md
+++ README.md
@@ -7,5 +7,14 @@
 - [Python Twitter Tools](https://github.com/sixohsix/twitter "https://github.com/sixohsix/twitter")
   - `pip install twitter`
 
+#### Set Environment Variables
+```
+$ export KINWU_API_KEY='Twitter API Key here'
+$ export KINWU_API_SECRET='Twitter API Secret here'
+$ export KINWU_ACCESS_TOKEN='Twitter Access Token here'
+$ export KINWU_ACCESS_TOKEN_SECRET='Twitter Access Token Secret here'
+$ export KINWU_DM_TO='User ID of DM destination from Kinwu'
+```
+
 ## Author
 [Kensuke Kosaka](https://github.com/trileg "https://github.com/trileg")
--- brain.py
+++ brain.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from twitter import *
-import configparser
 
 
 def brain():
-    config = configparser.ConfigParser()
-    config.read('twitter_key.ini')
-    api_key = config['apikey']['API_Key']
-    api_secret = config['apikey']['API_Secret']
-    access_token = config['apikey']['Access_Token']
-    access_token_secret = config['apikey']['Access_Token_Secret']
+    api_key = os.environ.get("KINWU_API_KEY")
+    api_secret = os.environ.get("KINWU_API_SECRET")
+    access_token = os.environ.get("KINWU_ACCESS_TOKEN")
+    access_token_secret = os.environ.get("KINWU_ACCESS_TOKEN_SECRET")
+    dm_to = os.environ.get("KINWU_DM_TO")
 
     while True:
         api = OAuth(access_token, access_token_secret, api_key, api_secret)
+        t = Twitter(auth=api)
         twitter_userstream = TwitterStream(auth=api, domain='userstream.twitter.com')
 
         for msg in twitter_userstream.user():
             if 'direct_message' in msg:
                 message = msg['direct_message']['text']
-                print(message)
+                if 'ping' in message:
+                    t.direct_messages.new(user=dm_to, text="pong")
 
 
 if __name__ == '__main__':
